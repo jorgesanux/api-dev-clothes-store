8acb75c0ef29961019343889ed802c45ba3b2e69
Jorge Sanabria <jorgesanux1@gmail.com>
2022-12-16 20:38:23 -0500

    Added support to object relations
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/common/interface/base-service.interface.ts
+++ src/common/interface/base-service.interface.ts
@@ -1,12 +1,12 @@
 export interface BaseServiceInterface<T = any, Y = any> {
     //TODO: Remove the ? optional
-    relations?: string[];
+    relations?: string[] | Object;
 
     //TODO: Remove T[] | Promise<T[]> and add [T[], number] only for findAll
     findAll(
         limit: number,
         page: number,
-        relations?: string[],
+        relations?: string[] | Object,
     ): T[] | Promise<T[]> | Promise<[T[], number]>;
     findOne(id: Y, relations?: string[]): T | Promise<T>;
     create(payload: any): T | Promise<T>;
