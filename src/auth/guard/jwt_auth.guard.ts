841ccad42cf5871bb153af61978e3abc5fdb54e6
Jorge Sanabria <jorgesanux1@gmail.com>
2022-12-21 21:12:22 -0500

    Added custome error managemnt in jwt strategy
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/auth/guard/jwt_auth.guard.ts
+++ src/auth/guard/jwt_auth.guard.ts
@@ -1,3 +1,24 @@
 import { AuthGuard } from "@nestjs/passport";
+import { BadRequestException, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        return super.canActivate(context);
+    }
+
+    handleRequest(err, user, info) {
+        if(err || !user) {
+            if(info instanceof TokenExpiredError){
+                throw new UnauthorizedException(`${info.message} at ${info.expiredAt.getTime()}`);
+            } else if(info instanceof JsonWebTokenError || info instanceof SyntaxError){
+                throw new BadRequestException(info.message);
+            }
+            throw err || new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
