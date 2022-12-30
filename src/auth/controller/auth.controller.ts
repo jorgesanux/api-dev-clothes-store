bb71402d5eeceb68c6ca35575aef5a36a0310dae
Jorge Sanabria <jorgesanux1@gmail.com>
2022-12-29 19:57:59 -0500

    Added signup method POST
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
 import { LocalAuthGuard } from '../guard/local_auth.guard';
 import { AuthService } from '../service/auth.service';
 import { JWTResponseModel } from '../model/jwt_response.model';
-import { User } from '../../user/entity/user.entity';
 import { ApiTags } from '@nestjs/swagger';
 import { Public } from '../decorator/public.decorator';
+import { SignupDTO } from '../dto/signup.dto';
+import { ApiResponse } from '../../common/interface/api_response.interface';
+import { Customer } from '../../user/entity/customer.entity';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -15,5 +25,16 @@ export class AuthController {
     @Post('/login')
     async login(@Request() req): Promise<JWTResponseModel> {
         return this.authService.generateToken(req.user);
+    }
+    @Public()
+    @HttpCode(HttpStatus.CREATED)
+    @Post('/signup')
+    async signup(@Body() body: SignupDTO): Promise<ApiResponse<Customer>> {
+        const response: ApiResponse<Customer> = {
+            statusCode: HttpStatus.CREATED,
+            message: 'Created',
+            result: await this.authService.registerUserAndCustomer(body),
+        };
+        return response;
     }
 }
